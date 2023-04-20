a8e9e7c742d5447e2cdaf7b41b8606281e2a3270
olmos2007 <olmosturaev5@gmail.com>
2023-04-20 10:46:09 +0500

    deploy-worker-project

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
